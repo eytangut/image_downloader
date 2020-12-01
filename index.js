9734648a12142941b2b7f63593a723fb477ac087
eytangut <eytangut@gmail.com>
2020-12-01 09:59:51 +0200

    1

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 console.log("my extension")
 alert("loaded")
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 var dom = document.getElementById("rso");
 console.log("the dom is" + dom.innerHTML)
 var links = document.getElementsByClassName("r");
