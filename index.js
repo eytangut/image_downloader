30880a12487b4823d468762a055e3df411ab7bc2
eytangut <eytangut@gmail.com>
2020-12-01 09:55:39 +0200

    alert index fix

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("my extension")
+alert("loaded")
+
+var dom = document.getElementById("rso");
+console.log("the dom is" + dom.innerHTML)
+var links = document.getElementsByClassName("r");
