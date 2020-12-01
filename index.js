e0514c8aefc4021d8a6babf079ece04e92af1919
eytangut <eytangut@gmail.com>
2020-12-01 10:06:46 +0200

    10:05 1/12/2020

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ alert("loaded")
 
 var dom = document.getElementById("rso");
 console.log("the dom is" + dom.innerHTML)
-var links = document.getElementsByClassName("r");
+var links = dom.getElementsByClassName("r");
+for(let i = 0; i<links.length; i++){
+    let titles = links[i].getElementsByTagName("h3")
+    console.log("titles is:" + titles)
+}
